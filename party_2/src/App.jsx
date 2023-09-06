e2080ff82073de3bb565900cc97a795ac0ad44e3
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-06 14:28:58 -0300

    Ajustando previamente o CSS

--- party_2/src/App.jsx
+++ party_2/src/App.jsx
@@ -8,11 +8,11 @@ function App() {
 
   return (
     <>
-      <h1>
+      <h1 className='title'>
         Balsa de Easthaven
       </h1>
 
-      <p>
+      <div>
         Uma carta encontrada em um posto avançado duergar secreto (veja “ O Invisível ”) pode levar os personagens 
         a este local. Entregar a carta ao Orador Waylen ou ao Capitão Arlaggath faz com que esses NPCs se reúnam e 
         determinem um curso de ação apropriado. Juntos, eles pedem aos personagens que vasculhem a balsa e matem 
@@ -20,18 +20,18 @@ function App() {
         acompanhará.
         
         Use a seguinte caixa de texto para descrever a balsa para os jogadores:
-        <span>
+        <p className='text-box'>
             A balsa de Easthaven, uma quilha, está presa no gelo na extremidade de um cais de madeira. 
             Uma pequena cabine está situada na extremidade traseira do casco.
-        </span>
+        </p>
         
         Quando o lago não está congelado, este barco transporta pessoas e cargas para as cidades de Caer-Dineval e 
         Caer-Konig por uma taxa modesta. Mas o barco e grande parte do porto de Easthaven estão presos no gelo, e 
         o Presidente Waylen declarou que o serviço de ferry deve cessar até que o gelo derreta – o que, dado o 
         temperamento de Auril, parece improvável que aconteça em breve. O proprietário e operador tiefling da balsa, 
-        <strong>Scython</strong>, passa seu tempo ocioso no Wet Trout (descrito abaixo). Na ausência de Scython, a 
+         <span className='blink-text'> Scython</span>, passa seu tempo ocioso no Wet Trout (descrito abaixo). Na ausência de Scython, a 
         balsa se tornou uma base de operações para um mestre mental duergar (veja apêndice C ) chamado 
-        <strong>Durth Sunblight</strong>. Ele e outros três duergars chamados <strong>Klaska, Ossyl e Zublorr</strong> 
+        <span className='blink-text'>Durth Sunblight</span>. Ele e outros três duergars chamados <span className='blink-text'>Klaska, Ossyl e Zublorr</span> 
         estão usando a cabine de popa da balsa como covil, mas apenas Durth está presente quando os personagens 
         chegam pela primeira vez à balsa. Os outros duergar estão procurando Chardalyn em Easthaven.
         
@@ -46,14 +46,14 @@ function App() {
       <h2>
           Cabine de popa
       </h2>
-      <span>
+      <p className='text-box'>
           O gelo que cobre a porta da cabine foi arrancado e a porta não está trancada. O espaço tem 
           aproximadamente 3 metros quadrados e contém quatro sacos de dormir, pacotes de rações espalhados e 
           um mapa enrolado do Vale do Vento Gélido que marca a localização de cada assentamento de Dez Cidades, 
           bem como a localização do posto avançado duergar próximo ao Sepulcro de Kelvin. (veja " Locais de 
           postos avançados ") e Sunblight, a fortaleza escondida nas montanhas (descrita no capítulo 3 ). 
           Todos os nomes de lugares no mapa estão escritos em anão.
-      </span>
+      </p>
         
         À espreita na cabana está Durth Sunblight. Ele sabe quando alguém embarca no navio pelo ranger do convés. Por precaução, ele fica invisível e fica em um canto vazio nos fundos da cabana. Se um intruso o detectar ou tentar roubar seu mapa, ele ataca. Se ele for reduzido a 20 pontos de vida ou menos, ele encolhe até o tamanho Minúsculo e foge através de um buraco de rato de 10 cm de diâmetro, saindo do navio 1,5 metro acima do porto congelado e 1,5 metro de distância do cais. Se ele escapar, Durth retorna para a fortaleza de seu pai nas montanhas (veja o capítulo 3 ), abandonando seus companheiros duergar.
 
@@ -67,7 +67,7 @@ function App() {
         Xardorok tem uma fortaleza escondida nas montanhas. (A rota para esta fortaleza está marcada no mapa de Durth a bordo da balsa.)
         A fortaleza de Xardorok possui uma forja alimentada pelo coração ainda pulsante de um dragão vermelho. Nesta forja, Xardorok está criando um dragão feito de chardalyn, com o qual planeja destruir Dez Cidades. O dragão chardalyn está perto de ser concluído.
         Xardorok tem outro filho, Nildar, que comanda um posto avançado secreto no lado nordeste do Cairn de Kelvin (veja " O Invisível ").
-      </p>
+      </div>
 
 
     </>
